ba82b174572ae80982ce45675554085ec57c8042
dwianiyuliaa <60848283+dwianiyuliaa@users.noreply.github.com>
2020-04-07 01:13:08 +0700

    Update CardBuilder.java
    add card builder

--- src/main/java/com/avatarduel/model/card/CardBuilder.java
+++ src/main/java/com/avatarduel/model/card/CardBuilder.java
@@ -1,8 +1,27 @@
 package com.avatarduel.model.card;
 
+import com.avatarduel.model.enums.Element;
+
 /**
  * Class yang bertanggung jawab untuk meng-construct suatu Card.
  */
 public class CardBuilder {
+    private Card card;
 
+    public CardBuilder(){
+        card = new Card();
+    }
+
+    public CardBuilder name(String name){
+        card.setName(name);
+    }
+    public CardBuilder description(String description){
+        card.setDescription(description);
+    }
+    public CardBuilder element(Element element){
+        card.setElement(element);
+    }
+    public Card build(){
+        return this.card;
+    }
 }
